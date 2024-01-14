2888fb903ca40ba43e041a45a09737ee8bde8d8e
Denis Semyannikov <Denny32191@gmail.com>
2024-01-14 23:57:39 +0200

    new commit

--- src/features/Personal/Personal.tsx
+++ src/features/Personal/Personal.tsx
@@ -3,12 +3,17 @@ import Wizard from "../../сomponents/Wizzard/Wizard";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { RootState } from "../../app/store";
-import { setNickName, setUserName, setSurName, setGender, setErrors } from './personalSlice'
+import {
+  setNickName,
+  setUserName,
+  setSurName,
+  setGender,
+  setErrors,
+} from "./personalSlice";
 import React from "react";
 import InputForm from "../../сomponents/InputForm/InputForm";
 import ButtonForm from "./../../сomponents/ButtonForm/ButtonForm";
 import DropDown from "../../сomponents/DropDown/DropDown";
-
 
 export const Personal = () => {
   const dispatch = useDispatch();
@@ -27,44 +32,51 @@ export const Personal = () => {
 
   const surname = useSelector((state: RootState) => state.personal.surname);
   const handleSurnameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    dispatch(setSurName(e.target.value))
+    dispatch(setSurName(e.target.value));
   };
-const gender = useSelector ((state:RootState) => state.personal.gender);
-const handleGender = (e: React.ChangeEvent<HTMLSelectElement>) => {
-  dispatch(setGender(e.target.value))
-}
-const errors = useSelector ((state: RootState) => state.personal.errors);
-const handleErrors = (e: React.ChangeEvent<HTMLInputElement>) => {
-  dispatch(setErrors({ nickname: "", username: "",surname: "",gender: "", }));
-}
+  const gender = useSelector((state: RootState) => state.personal.gender);
+  const handleGender = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    dispatch(setGender(e.target.value));
+  };
+  const errors = useSelector((state: RootState) => state.personal.errors);
+  const handleErrors = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(
+      setErrors({ nickname: "", username: "", surname: "", gender: "" })
+    );
+  };
 
- const navigate = useNavigate();
-const handleClick = (e: React.FormEvent<HTMLFormElement>) => {
-  e.preventDefault();
+  const navigate = useNavigate();
+  const handleClick = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-   if (nickname === "" && username === "" && surname === "" && gender === "") {
+    if (nickname === "" && username === "" && surname === "" && gender === "") {
       dispatch(
-     setErrors({ username: "Введите Имя", nickname: "Введите никнейм", surname: "Введите Фамилию", gender: "Выберите пол" })
-   );
-  
-  }  else if (username === "") {
-    dispatch(setErrors({ username: "Некорректное имя" }));
-    } else if ( nickname === "") {
-      dispatch(setErrors({  nickname: "Введите никнейм" }));
-   }else if (surname === "") {
-    dispatch(setErrors({surname: "Введите ФАмилию"}))
-   }else if (gender === "") {
-    dispatch(setErrors({gender: "Выберите Пол"}))
-   }
-    else {
-   dispatch(setErrors({ username: "", nickname: "", surname: "", gender: "" }));
-     navigate("/three");
+        setErrors({
+          username: "Введите Имя",
+          nickname: "Введите никнейм",
+          surname: "Введите Фамилию",
+          gender: "Выберите пол",
+        })
+      );
+    } else if (username === "") {
+      dispatch(setErrors({ username: "Некорректное имя" }));
+    } else if (nickname === "") {
+      dispatch(setErrors({ nickname: "Введите никнейм" }));
+    } else if (surname === "") {
+      dispatch(setErrors({ surname: "Введите Фамилию" }));
+    } else if (gender === "") {
+      dispatch(setErrors({ gender: "Выберите Пол" }));
+    } else {
+      dispatch(
+        setErrors({ username: "", nickname: "", surname: "", gender: "" })
+      );
+      navigate("/three");
     }
- };
+  };
   return (
     <div className={styles.personal}>
       <div className={styles.header}>
-      <Wizard/>
+        <Wizard />
       </div>
       <form onSubmit={handleClick} className={styles.form__block}>
         <InputForm
@@ -94,21 +106,20 @@ const handleClick = (e: React.FormEvent<HTMLFormElement>) => {
           onChange={handleSurnameChange}
           error={errors.surname}
         />
-       
-        <DropDown
-        onChange={handleGender}
-        error={errors.gender}
-        value={gender}
 
+        <DropDown
+          onChange={handleGender}
+          error={errors.gender}
+          value={gender}
         />
-        
+
         <div className={styles.button__block}>
-        <ButtonForm type="submit"  disabled={false}>
-          Назад
-        </ButtonForm>
-        <ButtonForm type="submit" disabled={false}>
-          Далее
-        </ButtonForm>
+          <ButtonForm type="submit" disabled={false}>
+            Назад
+          </ButtonForm>
+          <ButtonForm type="submit" disabled={false}>
+            Далее
+          </ButtonForm>
         </div>
       </form>
     </div>
--- src/сomponents/Dropdown/Dropdown.tsx
+++ src/сomponents/Dropdown/Dropdown.tsx
@@ -2,35 +2,38 @@ import { ChangeEvent } from "react";
 import styles from "./DropDown.module.scss";
 
 interface DropDownProps {
-  value?: string;
-  label?: string;
-    error?: string;
+  options?: {value: string, label: string}[];
+  value: string
+  error?: string;
     onChange: (e: ChangeEvent<HTMLSelectElement>) => void;
 }
 
 const DropDown: React.FC<DropDownProps> = ({
   error,
+  options,
   value,
-  label,
   onChange,
 }) => {
+ const genderOptions = [
+  {value: "male", label: 'Мужской'},
+  {value: "female", label: "Женский"},
+ ];
   return (
     <label  htmlFor="gender" className={styles.label__dropdown}>
       <select
         id="gender"
-        
-        
         onChange={onChange}
         className={styles.select__gender}
-
+        value={value}
       >
-        <option value="" disabled selected hidden>Выберите пол</option>
-        <option value="male" className={styles.option__male}>
-          Male
+        
+        <option value="" disabled hidden>
+          Выберите пол
         </option>
-        <option value="female" className={styles.option__female}>
-          Female
-        </option>
+        {genderOptions.map((options) => (
+          <option key={options.value} value={options.value}>{options.label}</option>
+        ))}
+        
       </select>
       {error && <p className={styles.error}>{error} </p>}
     </label>
